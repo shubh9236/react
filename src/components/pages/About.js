f476cc127e8950738ede9d8975216693fc976534
Shubham Agarwal <shubhamagarwal@SilverPushs-MacBook-Air-2.local>
2019-04-12 19:41:27 +0530

    helmet react unused

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,10 +3,10 @@ import { Helmet } from "react-helmet"
 function About() {
 	return(		
 		<React.Fragment>
-			// <Helmet>
-			//   <meta charSet="utf-8" />
-			//   <title>Inside about route</title>
-			// </Helmet>		
+			{/*<Helmet>
+			  <meta charSet="utf-8" />
+			  <title>Inside about route</title>
+			</Helmet>*/}		
 			<h1>About</h1>
 			<p>This is the todo lsit app version 1.0.0. This is part of react crash course</p>
 		</React.Fragment>
