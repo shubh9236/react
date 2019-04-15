6c3c257b6a7a8c2d033c0a469c2c1d841eb19211
Shubham Agarwal <shubhamagarwal@Shubham.local>
2019-04-15 13:53:21 +0530

    component for seo

--- src/components/layouts/SeoHead.js
+++ src/components/layouts/SeoHead.js
@@ -4,14 +4,16 @@ const defaultDescription = "To do list app creator"
 const defaultUrl = "shubh9236.github.io/react"
 const SeoHead = props =>{
 	return(
-		<Helmet>
-			<meta charSet="utf-8" />
-			<title>{props.title || ""}</title>
-			<meta name="description" content={props.description || defaultDescription} />
-			<meta name="viewport" content="width=device-width, initial-scale=1" />
-			<meta property = "url" content={props.url || defaultUrl} />
-			<meta property = "title" content={ptops.title || ""}>
-		</Helmet>
+		<React.Fragment>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<title>{props.title || ""}</title>
+				<meta name="description" content={props.description || defaultDescription} />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta property = "url" content={props.url || defaultUrl} />
+				<meta property = "title" content={ptops.title || ""}>
+			</Helmet>
+		</React.Fragment>
 	)
 }
 
