6af7f7cc82616630b708daf5725e51815292630b
Shubham Agarwal <shubhamagarwal@Shubham.local>
2019-04-15 13:56:12 +0530

    Updates

---
+++ precache-manifest.6e5e29f6c228c9ce143168be8e58338a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  },
+  {
+    "revision": "7b9aa32fbac74cb03887",
+    "url": "/react/static/js/main.7999cc16.chunk.js"
+  },
+  {
+    "revision": "da355a48ddb5cf4cd108",
+    "url": "/react/static/js/2.8ffb6e37.chunk.js"
+  },
+  {
+    "revision": "7b9aa32fbac74cb03887",
+    "url": "/react/static/css/main.f0df9ab0.chunk.css"
+  },
+  {
+    "revision": "da2279f2d39151cc791c66706f32a53d",
+    "url": "/react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react/precache-manifest.d30253a59eba48e6ae8937ecf5ef4797.js"
+  "/react/precache-manifest.6e5e29f6c228c9ce143168be8e58338a.js"
 );
 
 workbox.clientsClaim();
